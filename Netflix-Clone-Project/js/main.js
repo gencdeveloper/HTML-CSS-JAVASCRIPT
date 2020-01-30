becba1d47c76bdfb36e5fd59c5bce8bb00ccb3be
Emre <43030772+gencdeveloper@users.noreply.github.com>
2020-01-30 03:40:34 +0100

    Add files via upload

---
+++ Netflix-Clone-Project/js/main.js
@@ -0,0 +1,29 @@
+// Grab all tab & tab content items to create node list (like an array - so can loop thru)
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+ // Remove all show and border classes
+ removeBorder();
+ removeShow();
+ // Add border to current tab item
+ this.classList.add('tab-border');
+ // Grab content item from DOM
+ const tabContentItem = document.querySelector(`#${this.id}-content`);
+ // Add show class
+ tabContentItem.classList.add('show');
+}
+
+// Remove border function
+function removeBorder() {
+ tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+// Remove the 'show' class to change content displayed
+function removeShow() {
+ tabContentItems.forEach(item => item.classList.remove("show"));
+}
+
+// Loop through the nodelist(array) & listen for event
+tabItems.forEach(item => item.addEventListener("click", selectItem));
